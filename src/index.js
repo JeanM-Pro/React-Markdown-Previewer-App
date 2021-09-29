91334dcd6798e0f05ab6c8c5580e040c582aa723
JeanM-Pro <jeancenteno54@gmail.com>
2021-09-28 23:14:19 -0500

    se creo el componente padre

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { MarkdownPreviewerApp } from './MarkdownPreviewerApp';
 
+const divRoot = document.querySelector('#root')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render( <MarkdownPreviewerApp />, divRoot );
 
 
