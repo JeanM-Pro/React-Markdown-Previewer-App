84c5a98c3d69b5f592872918dd19f5dc016bde1c
JeanM-Pro <jeancenteno54@gmail.com>
2021-09-29 10:28:26 -0500

    se creo el borrador

--- src/MarkdownPreviewerApp.js
+++ src/MarkdownPreviewerApp.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Markdown from 'markdown-to-jsx';
 
 export const MarkdownPreviewerApp = () => {
+
+    const [markdownText, setMarkdownText] = useState('')
+
+
     return (
+
         <div>
+            <div className="app" style={{ display: 'flex' }}>
             
+                <textarea id='editor' value={markdownText} onChange={(event) => setMarkdownText(event.target.value)}/>
+            
+                <div className="markdown" id='preview'>
+                
+                    <Markdown>{markdownText}</Markdown>
+                    
+            
+                </div>
+    
+            </div>
+
         </div>
     )
 }
