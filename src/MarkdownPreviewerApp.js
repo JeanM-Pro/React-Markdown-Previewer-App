33f1fefeba07a4019f07ac3e69975a9b91cb9ffe
JeanM-Pro <jeancenteno54@gmail.com>
2021-09-30 16:01:13 -0500

    proyecto terminado

--- src/MarkdownPreviewerApp.js
+++ src/MarkdownPreviewerApp.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react'
 import Markdown from 'markdown-to-jsx';
+import { initialState } from './data';
+
 
 export const MarkdownPreviewerApp = () => {
 
-    const [markdownText, setMarkdownText] = useState('')
+
+    const [markdownText, setMarkdownText] = useState(initialState)
 
 
     return (
 
-        <div>
+        <div className='content'>
+            
+            <div className='title'>Convert your Markdown</div>
+
             <div className="app" style={{ display: 'flex' }}>
-            
-                <textarea id='editor' value={markdownText} onChange={(event) => setMarkdownText(event.target.value)}/>
-            
-                <div className="markdown" id='preview'>
                 
-                    <Markdown>{markdownText}</Markdown>
-                    
-            
+                <div className='content-box'>
+                    <h3>Enter your Markdown:</h3>
+                    <textarea id='editor' value={markdownText} onChange={(event) => setMarkdownText(event.target.value)}/>
                 </div>
-    
+
+                <div className='content-box'>
+
+                    <h3>See the result:</h3>
+                    <div className='preview-styles' id='preview'>
+
+                        <span>  <Markdown>{markdownText}</Markdown> </span>
+                
+                    </div>
+
+                </div>
+                   
             </div>
+
+            
 
         </div>
     )
